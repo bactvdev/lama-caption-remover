ce93217589dc2c3b0c174d8211f5310713179770
Bac Truong Van - Windows <bactruongvan17@gmail.com>
2025-07-14 21:39:25 +0700

    u

---
+++ install.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+echo "📦 Installing Python dependencies..."
+pip install -r requirements.txt
+
+mkdir -p lama/models/big-lama/models
+
+# ID của file trên Google Drive (thay bằng ID thật của bạn)
+CONFIG_ID="1sw6eLiCJb8ngKCEBczrtJyajDyGbWBVh"
+CKPT_ID="1MBjuLv9HphGH1CKlKxwweTdZEXnkAdmB"
+
+# Download config.yaml
+if [ ! -f lama/models/big-lama/config.yaml ]; then
+    echo "⬇️  Downloading config.yaml..."
+    gdown --id $CONFIG_ID -O models/big-lama/config.yaml
+else
+    echo "✅ config.yaml already exists."
+fi
+
+# Download best.ckpt
+if [ ! -f lama/models/big-lama/models/best.ckpt ]; then
+    echo "⬇️  Downloading best.ckpt..."
+    gdown --id $CKPT_ID -O models/big-lama/models/best.ckpt
+else
+    echo "✅ best.ckpt already exists."
+fi
+
+pip install -r requirements.txt
+
+# ======================
+# 3. Cài đặt Ngrok CLI
+# ======================
+if ! command -v ngrok &> /dev/null; then
+    wget -q -O ngrok.zip https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-stable-linux-amd64.zip
+    unzip -o ngrok.zip
+else
+    echo "✅ Ngrok already installed."
+fi
+
+./ngrok config add-authtoken 2zrm3wOOXwW7QdbVajnB1zt1jgc_44gW91d6M8Y1ms9WYtpMs
+
+uvicorn main:app --host 0.0.0.0 --port 8000
+
+echo "✅ Done setting up!"
+
+# chmod +x install.sh
